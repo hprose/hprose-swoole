080d6bd18e5199b388945d1a4ffaa4388236abe3
Ma Bingyao <mabingyao@gmail.com>
2016-07-21 12:29:42 +0800

    Update .travis.yml

---
+++ install_ext.sh
@@ -0,0 +1,9 @@
+wget https://pecl.php.net/get/swoole-1.8.7.tgz
+tar zxvfp lswoole-1.8.7.tgz
+cd swoole-1.8.7
+phpize
+./configure
+make
+make install
+echo "extension = libswoole.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+cd ..
