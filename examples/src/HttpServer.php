a3982c168f0dbddf026683940e93c840d27278a0
Ma Bingyao <mabingyao@gmail.com>
2016-07-25 14:17:02 +0800

    Added Some Examples.

---
+++ examples/src/HttpServer.php
@@ -0,0 +1,13 @@
+<?php
+require_once "../vendor/autoload.php";
+
+use Hprose\Swoole\Server;
+
+function hello($name) {
+    return "Hello $name!";
+}
+$server = new Server("http://0.0.0.0:8086");
+$server->setErrorTypes(E_ALL);
+$server->setDebugEnabled();
+$server->addFunction('hello');
+$server->start();
---
+++ examples/src/UnixServer.php
@@ -0,0 +1,13 @@
+<?php
+require_once "../vendor/autoload.php";
+
+use Hprose\Swoole\Server;
+
+function hello($name) {
+    return "Hello $name!";
+}
+$server = new Server("unix:/tmp/my.sock");
+$server->setErrorTypes(E_ALL);
+$server->setDebugEnabled();
+$server->addFunction('hello');
+$server->start();
