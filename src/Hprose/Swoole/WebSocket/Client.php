2e88fff3aaf467dd27f783962716bd8d315dca1e
Ma Bingyao <mabingyao@gmail.com>
2016-08-06 00:25:59 +0800

    Changed getKeepAlive to isKeepAlive

--- src/Hprose/Swoole/WebSocket/Client.php
+++ src/Hprose/Swoole/WebSocket/Client.php
@@ -14,7 +14,7 @@
  *                                                        *
  * hprose swoole websocket client library for php 5.3+    *
  *                                                        *
- * LastModified: Jul 25, 2016                             *
+ * LastModified: Aug 6, 2016                              *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
@@ -69,7 +69,7 @@ class Client extends \Hprose\Client {
             unset($this->header['Keep-Ailve']);
         }
     }
-    public function getKeepAlive() {
+    public function isKeepAlive() {
         return $this->keepAlive;
     }
     public function setKeepAliveTimeout($timeout) {
