4e3de335985af263a86971662e9f5c33b2722da0
Ma Bingyao <mabingyao@gmail.com>
2016-12-05 21:11:15 +0800

    Update composer.json

--- install_ext.sh
+++ install_ext.sh
@@ -6,4 +6,12 @@ phpize
 make
 make install
 echo "extension = swoole.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+wget https://pecl.php.net/get/hprose-1.6.5.tgz
+tar zxvfp hprose-1.6.5.tgz
+cd hprose-1.6.5
+phpize
+./configure
+make
+make install
+echo "extension = hprose.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 cd ..
